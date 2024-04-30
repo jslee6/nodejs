261373764e06c1855d41d4bc972af25539f02d9d
이진석 <gkrryrwkk6@naver.com>
2024-04-30 18:05:21 +0900

    Mongo DB_conect

--- server.js
+++ server.js
@@ -8,6 +8,19 @@ app.use(express.static(__dirname + '/public'))
 // 퍼블릭파일 등록방법
 
 
+const { MongoClient } = require('mongodb')
+
+let db
+const url = 'mongodb+srv://jslee:qlalf1@cluster0.wqytvou.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+new MongoClient(url).connect().then((client)=>{
+  console.log('DB연결성공')
+  db = client.db('forum')
+}).catch((err)=>{
+  console.log(err)
+}) 
+// 몽고DB 라이브러리 사용법 , 몽고DB에 커넥트하면 포럼 DB에 접속해주세요~ ,  url 맨위에  계정:암호
+
+
 app.listen(8080, () => {
     console.log('http://localhost:8080 에서 서버 실행중')
 })  // 서버만드는 코드
@@ -19,6 +32,8 @@ app.get('/', (요청, 응답) => {
 
 
 
-app.get('/news',function(요청, 응답) {
-  응답.send('오늘비움')
-})    // /news 페이지로 라우트  ,펑션 or 애로우평션  , 콜백함수
+app.get('/news', ()=>{
+  // db.collection('post').insertOne({title : '어쩌구'}) // 테스트이여서 주석처리
+})
+  // 응답.send('오늘비움')
+   // /news 페이지로 라우트  ,펑션 or 애로우평션  , 콜백함수
